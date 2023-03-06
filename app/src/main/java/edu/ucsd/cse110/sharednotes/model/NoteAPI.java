bbfc962d9f7095f2ecddb87d3a216d205d489baa
asunboi <as.alexander.sun@gmail.com>
2023-03-06 12:19:08 -0800

    Implemented Okhttp API and made get / pull requests in NoteAPI

--- app/src/main/java/edu/ucsd/cse110/sharednotes/model/NoteAPI.java
+++ app/src/main/java/edu/ucsd/cse110/sharednotes/model/NoteAPI.java
@@ -11,19 +11,25 @@ import com.google.gson.Gson;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class NoteAPI {
-    // TODO: Implement the API using OkHttp!
-    // TODO: - getNote (maybe getNoteAsync)
-    // TODO: - putNote (don't need putNotAsync, probably)
+    // DONE: Implement the API using OkHttp!
+    // DONE: - getNote (maybe getNoteAsync)
+    // DONE: - putNote (don't need putNotAsync, probably)
     // TODO: Read the docs: https://square.github.io/okhttp/
     // TODO: Read the docs: https://sharednotes.goto.ucsd.edu/docs
 
     private volatile static NoteAPI instance = null;
 
     private OkHttpClient client;
+
+    public static final MediaType JSON
+            = MediaType.get("application/json; charset=utf-8");
 
     public NoteAPI() {
         this.client = new OkHttpClient();
@@ -72,5 +78,53 @@ public class NoteAPI {
 
         // We can use future.get(1, SECONDS) to wait for the result.
         return future;
+    }
+
+    @WorkerThread
+    public String getNote(Note note) {
+        String url = note.title;
+        String encodedURL = url.replace(" ", "%20");
+
+        var request = new Request.Builder()
+                .url("https://sharednotes.goto.ucsd.edu/echo/" + encodedURL)
+                .build();
+
+        try (var response = client.newCall(request).execute()) {
+            assert response.body() != null;
+            var body = response.body().string();
+            Log.i(url, body);
+            return body;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @AnyThread
+    public Future<String> getNoteAsync(Note note) {
+        var executor = Executors.newSingleThreadExecutor();
+        var future = executor.submit(() -> getNote(note));
+
+        // We can use future.get(1, SECONDS) to wait for the result.
+        return future;
+    }
+
+    public String putNote(Note note){
+        String url = note.title;
+        String encodedURL = url.replace(" ", "%20");
+
+        RequestBody body = RequestBody.create(note.toJSON(), JSON);
+
+        Request request = new Request.Builder()
+                .url(encodedURL)
+                .post(body)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
